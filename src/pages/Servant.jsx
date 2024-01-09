3b37274a89d45b7b8f6f5e8f51ff16e113d07218
Thierry Vicente <119328995+StalkerVince@users.noreply.github.com>
2024-01-09 19:09:36 +0100

    improved character details

--- src/pages/Servant.jsx
+++ src/pages/Servant.jsx
@@ -5,14 +5,27 @@ function Servant() {
   const { data } = useLoaderData();
   console.log(data);
 
+  console.log(data.traits);
   return (
-    <div>
-      <NavLink to={`/list`}>Home</NavLink>
-      <h1>{data.name}</h1>
-      <img src={data.extraAssets.charaGraph.ascension[1]} alt={data.name} />
-      <p>{data.id}</p>
-    </div>
+    <>
+      <div>
+        <NavLink to={`/list`}>Home</NavLink>
+        <h1>{data.name}</h1>
+        <p>{data.originalName}</p>
+        <img src={data.extraAssets.charaGraph.ascension[0]} />
+        <div>
+          <img src={data.extraAssets.charaGraph.ascension[1]} />
+        </div>
+        <h2 className="capitalize">Class: {data.className}</h2>
+        <h2>Rarity: {data.rarity}⭐</h2>
+        <p>Gender: {data.gender}</p>
+        <p>{data.traits[3].name}</p>
+        <p>{data.traits[4].name}</p>
+      </div>
+    </>
   );
 }
 
 export default Servant;
+
+//<p>{data.traits.find((e) => e.id === 2795).name}</p>
